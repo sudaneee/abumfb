f9ab190900f462f34af74da79f46c5aae7775cbe
sudaneee <99607513+sudaneee@users.noreply.github.com>
2026-01-10 19:24:18 +0100

    Update settings.py
    update

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -124,6 +124,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+testing = "My Test"
 
 
 # Default primary key field type
